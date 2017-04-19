e7b68228c580d70f05c3b4d15d7d1725f787c538
Adiel Hercules <jadher.11x2@gmail.com>
2017-04-18 23:30:28 -0600

    Added Landing Hero Image
    Also added services section, removed contact section (will be a page)

--- src/containers/landing/index.js
+++ src/containers/landing/index.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import Scrollchor from 'react-scrollchor';
 import styled from 'styled-components';
+import { Col, Row } from 'react-styled-flexboxgrid';
 
 import Signup from '../../components/account/signup';
 
@@ -10,9 +11,11 @@ const MainMenuComponent = ({ isFixed = false }) => (
     <Logotype to="/">CLIFRE</Logotype>
 
     <Menu>
-      {isFixed && <MenuItem><Scrollchor to="">inicio</Scrollchor></MenuItem>}
-      <MenuItem><Scrollchor to="features" animate={{ offset: -10 }}>caracterisitcas</Scrollchor></MenuItem>
-      <MenuItem><Scrollchor to="contact" animate={{ offset: -60 }}>contacto</Scrollchor></MenuItem>
+      <If condition={isFixed}>
+        <MenuItem><Scrollchor to="">inicio</Scrollchor></MenuItem>
+      </If>
+      <MenuItem><Scrollchor to="services">servicios</Scrollchor></MenuItem>
+      <MenuItem><Scrollchor to="features" animate={{ offset: -60 }}>caracterisitcas</Scrollchor></MenuItem>
       <Divider />
       <MenuItem><Link to="/account/signin" activeClassName="active">entrar</Link></MenuItem>
       <MenuItem><Scrollchor to="signup" animate={{ offset: -60 }} className="button-like">registrarme</Scrollchor></MenuItem>
@@ -71,7 +74,10 @@ class LandingPage extends Component {
               <MainMenuComponent />
             </MainMenu>
 
-            <Wrapper>
+            <Wrapper relative fullHeight>
+              <HeroImage>
+                <img src="https://placeholdit.imgix.net/~text?txtsize=33&bg=d3d3d3&txtclr=000000&txt=clifre&w=720&h=480" alt=""/>
+              </HeroImage>
               <Hero id="hero">
                 <div className="hero-cell">
                   <h1>Tus clientes tu exito</h1>
@@ -101,47 +107,92 @@ class LandingPage extends Component {
               <MainMenuComponent isFixed />
             </FixedMainMenu>
 
-            <SignupBar id="features">
+            <Services id="services">
               <Wrapper>
-                <h1>Features</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, saepe nisi explicabo
-                voluptatibus facilis ullam incidunt omnis repudiandae molestias iste unde ea doloribus nam sit
-                vel, veritatis eius, in earum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, saepe nisi explicabo
-                voluptatibus facilis ullam incidunt omnis repudiandae molestias iste unde ea doloribus nam sit
-                vel, veritatis eius, in earum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, saepe nisi explicabo
-                voluptatibus facilis ullam incidunt omnis repudiandae molestias iste unde ea doloribus nam sit
-                vel, veritatis eius, in earum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, saepe nisi explicabo
-                voluptatibus facilis ullam incidunt omnis repudiandae molestias iste unde ea doloribus nam sit
-                vel, veritatis eius, in earum.</p>
-              </Wrapper>
-            </SignupBar>
+                <h2>Fideliza tus clientes frecuentes en segundos&hellip;</h2>
 
-            <SignupBar id="contact">
-              <Wrapper>
-                <h1>Contact</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, saepe nisi explicabo
-                voluptatibus facilis ullam incidunt omnis repudiandae molestias iste unde ea doloribus nam sit
-                vel, veritatis eius, in earum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, saepe nisi explicabo
-                voluptatibus facilis ullam incidunt omnis repudiandae molestias iste unde ea doloribus nam sit
-                vel, veritatis eius, in earum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, saepe nisi explicabo
-                voluptatibus facilis ullam incidunt omnis repudiandae molestias iste unde ea doloribus nam sit
-                vel, veritatis eius, in earum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, saepe nisi explicabo
-                voluptatibus facilis ullam incidunt omnis repudiandae molestias iste unde ea doloribus nam sit
-                vel, veritatis eius, in earum.</p>
+                <Row className="services">
+                  <Col xs={6} md={4} className="services__item">
+                    <h1>1</h1>
+                    <h3>Crea una cuenta</h3>
+                    <p>Crea una cuenta (es gratis) y accede con tus credenciales.</p>
+                  </Col>
+                  <Col xs={6} md={4} className="services__item">
+                    <h1>2</h1>
+                    <h3>Crea una tarjeta</h3>
+                    <p>Crea y personaliza una tarjeta en menos de 5 minutos.</p>
+                  </Col>
+                  <Col xs={6} md={4} className="services__item">
+                    <h1>3</h1>
+                    <h3>Fideliza tus clientes</h3>
+                    <p>Comparte la tarjeta con tus clientes y empieza a fidelizar.</p>
+                  </Col>
+                </Row>
               </Wrapper>
-            </SignupBar>
+            </Services>
+
+            <Features id="features">
+              <Wrapper>
+                <h2>Todo lo que necesita</h2>
+              </Wrapper>
+            </Features>
 
             <SignupBar id="signup">
               <Wrapper>
                 <Signup />
               </Wrapper>
             </SignupBar>
+
+            <Footer>
+              <Wrapper>
+                <Row className="footer">
+                  <Col md={3}>
+                    <Link to="/" className="logotype">CLIFRE</Link>
+                    <p>Programa de fidelizacion y control de clientes frecuentes.</p>
+                  </Col>
+                  <Col md={9}>
+                    <Row>
+                      <Col md={3}>
+                        <ul className="footer__menu">
+                          <li className="footer__menu__item--heading">Saber mas</li>
+                          <li className="footer__menu__item"><Scrollchor to="services">Como funciona?</Scrollchor></li>
+                          <li className="footer__menu__item"><Scrollchor to="features">Caracteristicas</Scrollchor></li>
+                          <li className="footer__menu__item"><Link to="/sitemap">Mapa del Sitio</Link></li>
+                        </ul>
+                      </Col>
+                      <Col md={3}>
+                        <ul className="footer__menu">
+                          <li className="footer__menu__item--heading">Soporte</li>
+                          <li className="footer__menu__item"><Scrollchor to="/services">Como funciona?</Scrollchor></li>
+                          <li className="footer__menu__item"><Scrollchor to="/features">Caracteristicas</Scrollchor></li>
+                          <li className="footer__menu__item"><Link to="/sitemap">Mapa del Sitio</Link></li>
+                        </ul>
+                      </Col>
+                      <Col md={3}>
+                        <ul className="footer__menu">
+                          <li className="footer__menu__item--heading">Nosotros</li>
+                          <li className="footer__menu__item"><Scrollchor to="/services">Como funciona?</Scrollchor></li>
+                          <li className="footer__menu__item"><Scrollchor to="/features">Caracteristicas</Scrollchor></li>
+                          <li className="footer__menu__item"><Link to="/sitemap">Mapa del Sitio</Link></li>
+                        </ul>
+                      </Col>
+                      <Col md={3}>
+                        <div>
+                          <Scrollchor to="signup">Crear cuenta (gratis)</Scrollchor>
+                        </div>
+                        <div>
+                          o <Link to="/account/signup">Entrar</Link>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+
+                <div className="footer-copyright">
+                  Todos los derechos reservados. Clifre { new Date().getFullYear() }
+                </div>
+              </Wrapper>
+            </Footer>
           </PageContent>
         </Page>
       </div>
@@ -194,6 +245,23 @@ const Wrapper = styled.div`
   max-width: 1200px;
   margin: 0 auto;
   padding: 0 30px;
+
+  ${props => {
+    let extraCss = '';
+    if (props.relative) {
+      extraCss += `
+        position: relative;
+      `;
+    }
+
+    if (props.fullHeight) {
+      extraCss += `
+        height: 100%;
+      `;
+    }
+
+    return extraCss;
+  }}
 `;
 
 const Content = styled.div`
@@ -211,9 +279,13 @@ const Logotype = styled(Link)`
 
 const MainMenu = styled.div`
   position: relative;
-  margin: 40px 0 0;
+  margin: 0;
+  padding-top: 40px;
   padding-bottom: 20px;
   text-align: right;
+  background-color: #F9AE00;
+  background-color: rgba(249, 174, 0, 0.9);
+  z-index: 2;
   transition: opacity 0.3s ease-in;
 
   &.is-hidden {
@@ -233,6 +305,7 @@ const FixedMainMenu = styled.div`
   background-color: #2b2b2b;
   transform: translateY(-100%);
   transition: transform 0.3s ease-in;
+  z-index: 99;
 
   &.is-visible {
     transform: translateY(0);
@@ -252,8 +325,9 @@ const MenuItem = styled.li`
   a {
     display: block;
     padding: 5px 0;
-    margin: 0 15px;
+    margin: 0 14px;
     border-bottom: 2px solid transparent;
+    font-size: 14px;
     text-transform: uppercase;
     text-decoration: none;
     font-weight: bold;
@@ -318,7 +392,8 @@ const PageContent = styled.div`
 `;
 
 const SignupBar = styled.div`
-  padding: 50px 0;
+  padding: 100px 0;
+  background-color: #F9AE00;
 `;
 
 const GettingStartedButton = styled(Scrollchor)`
@@ -343,6 +418,8 @@ const Hero = styled.div`
   display: table;
   min-height: 300px;
   max-height: 500px;
+  width: 48%;
+  padding-bottom: 150px;
   transform: translateY(-50%);
 
   .hero-cell {
@@ -364,6 +441,81 @@ const Hero = styled.div`
       padding: 20px 0;
     }
   }
+`;
+
+const HeroImage = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 52%;
+  width: 48%;
+  padding-bottom: 150px;
+  transform: translateY(-50%);
+
+  img {
+    display: block;
+  }
+`;
+
+const Services = styled.div`
+  padding: 100px 0;
+
+  h2 {
+    display: block;
+    max-width: 400px;
+    color: #F9AE00;
+    font-weight: 500;
+    line-height: 1.2em;
+
+    &:after {
+      content: "";
+      display: block;
+      margin-top: 20px;
+      width: 80px;
+      height: 3px;
+      background-color: #4110AA;
+    }
+  }
+
+  .services {
+    margin-top: 50px;
+
+    &__item {
+      display: block;
+      margin: 0 auto;
+      max-width: 300px;
+      padding-bottom: 100px;
+
+      h1 {
+        display: block;
+        text-align: center;
+        margin: 0;
+        padding: 25px 50px 25px 0;
+        color: #999;
+        font-size: 200px;
+        font-weight: 500;
+        opacity: 0.5;
+      }
+
+      h3 {
+        color: #F9AE00;
+        font-weight: 500;
+      }
+
+      p {
+        margin-top: 30px;
+        color: #909090;
+      }
+    }
+  }
+`;
+
+const Features = styled.div`
+  padding: 100px 0;
+  background-color: #eef5f9;
+`;
+
+const Footer = styled.div`
+  padding: 100px 0;
 `;
 
 export default LandingPage;
